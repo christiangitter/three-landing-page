275fd7e40f31dc295bcd1f04688453236b796ba8
Christian Gitter <gitter.christian@yahoo.de>
2022-01-05 21:58:04 +0100

    center the bottle

--- js/app.js
+++ js/app.js
@@ -18,7 +18,7 @@ function init() {
 
     //Camera Setup 
     camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
-    camera.position.set(0, 20, 120);
+    camera.position.set(18, 8, 120);
     
     //set Hintergrundbeleuchtung
     const ambient = new THREE.AmbientLight(0xffff00,30)
